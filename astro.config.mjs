8b9a287102f626df79b952a9096fd2e89b543684
Beöthy Bence <beothy.bence@gmail.com>
2025-08-01 22:13:03 +0200

    Set base path for deployment in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
+  base: '/OctoGon-3D-Portfolio-Page/',
   vite: {
     plugins: [tailwindcss()]
   }
